7ba610ced12b1bf3d664b0fca44915153fba36c1
Vtom7 <toms.avenge02@gmail.com>
2026-04-21 17:00:10 +0200

    feat: amélioration de la migration

--- migration.js
+++ migration.js
@@ -1,132 +1,96 @@
 const { Sequelize, DataTypes } = require('sequelize');
+
 const sequelize = new Sequelize('projet_quiz_musical', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
 });
 
+/* =========================
+   MODELES
+========================= */
+
 const Quiz = sequelize.define('Quiz', {
-    titre: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-    },
-    id_question: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-    },
-    id_resultat: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-    }
+  titre: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  }
 });
 
 const Question = sequelize.define('Question', {
-    ordre_question: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+  contenu: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  id_quiz: {
+    type: DataTypes.INTEGER, // ⚠️ DOIT matcher Quiz.id
+    allowNull: false,
+    references: {
+      model: Quiz,
+      key: 'id'
     },
-    texte_question: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    id_quiz: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-    }
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  }
 });
 
 const Reponse = sequelize.define('Reponse', {
-    texte_reponse: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+  reponse: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  id_question: {
+    type: DataTypes.INTEGER, // ⚠️ DOIT matcher Quiz.id
+    allowNull: false,
+    references: {
+      model: Question,
+      key: 'id'
     },
-    correcte: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false
-    },
-    id_question: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-    }
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  }
 });
 
-const Resultat = sequelize.define('Resultat', {
-    score: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    total: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    pourcentage: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    rang: {
-        type: DataTypes.STRING(5),
-        allowNull: false
-    },
-    message: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-    },
-    date_resultat: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    id_quiz: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-    }
-});
 
 /* =========================
    RELATIONS
 ========================= */
 
-// QUIZ -> QUESTION
 Quiz.hasMany(Question, {
-    foreignKey: 'id_quiz'
-})
+  foreignKey: 'id_quiz'
+});
 
 Question.belongsTo(Quiz, {
-    foreignKey: 'id_quiz'
-})
+  foreignKey: 'id_quiz'
+});
 
-// QUESTION -> REPONSE
 Question.hasMany(Reponse, {
-    foreignKey: 'id_question'
-})
+  foreignKey: 'id_question'
+});
 
 Reponse.belongsTo(Question, {
-    foreignKey: 'id_question'
-})
+  foreignKey: 'id_question'
+});
 
-// QUIZ -> RESULTAT
-Quiz.hasMany(Resultat, {
-    foreignKey: 'id_quiz'
-})
-
-Resultat.belongsTo(Quiz, {
-    foreignKey: 'id_quiz'
-})
 
 /* =========================
    EXPORT
 ========================= */
 
 module.exports = {
-    sequelize,
-    Quiz,
-    Question,
-    Reponse,
-    Resultat
-}
+  sequelize,
+  Quiz,
+  Question,
+  Reponse
+};
+
+/* =========================
+   SYNC DB
+========================= */
 
 sequelize.sync()
   .then(() => {
-	console.log('Database & tables created!');
-	  })
-	    .catch(err => {
-	console.error('Unable to connect to the database:', err);
-	  });
+    console.log('Database & tables created!');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
